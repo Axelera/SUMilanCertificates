883babd90445fb07ed4b2f02571d38d4c6769a0d
Luca8991 <luca.berte98@hotmail.it>
2021-11-05 17:53:46 +0100

    feat: retrieve tokens also by address (enumerable)

--- contracts/SUMilanCertificateUpgradeable.sol
+++ contracts/SUMilanCertificateUpgradeable.sol
@@ -1,14 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 
 /// @custom:security-contact info@singularityumilan.com
-contract SUMilanCertificate is Initializable, ERC721URIStorageUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
+contract SUMilanCertificate is Initializable, ERC721Upgradeable, ERC721URIStorageUpgradeable, ERC721EnumerableUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
     using CountersUpgradeable for CountersUpgradeable.Counter;
 
     CountersUpgradeable.Counter private _tokenIdCounter;
@@ -28,7 +30,7 @@ contract SUMilanCertificate is Initializable, ERC721URIStorageUpgradeable, Ownab
         __UUPSUpgradeable_init();
     }
 
-    function mintCertificate(address to, string memory tokenURI, string memory eventId)
+    function mintCertificate(address to, string memory tokenUri, string memory eventId)
         public onlyOwner
         returns (uint256)
     {
@@ -42,7 +44,7 @@ contract SUMilanCertificate is Initializable, ERC721URIStorageUpgradeable, Ownab
 
         uint256 newItemId = _tokenIdCounter.current();
         _mint(to, newItemId);
-        _setTokenURI(newItemId, tokenURI);
+        _setTokenURI(newItemId, tokenUri);
         _certificateOwners[to].eventIds[eventId] = true;
 
         return newItemId;
@@ -53,4 +55,38 @@ contract SUMilanCertificate is Initializable, ERC721URIStorageUpgradeable, Ownab
         onlyOwner
         override
     {}
+    
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721Upgradeable, ERC721EnumerableUpgradeable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721Upgradeable, ERC721EnumerableUpgradeable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+    
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+    {
+        super._burn(tokenId);
+    }
+    
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
 }
